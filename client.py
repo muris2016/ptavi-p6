0adf693ff26eabd01b2bfed7c309235649c3deef
muris2016 <rsemuris2016@gmail.com>
2015-11-23 12:15:12 +0100

    Primera version

--- client.py
+++ client.py
@@ -1,32 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """
 Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+if __name__ == "__main__":
+    METHOD = sys.argv[1]
+    LOGIN = sys.argv[2].split('@')[0]
+    IP = sys.argv[2].split('@')[1].split(':')[0]
+    PORT = int(sys.argv[2].split(':')[-1])
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+    if METHOD == 'INVITE' or METHOD == 'BYE':
+        MSG = '%s sip:%s@%s SIP/2.0\r\n\r\n' % (METHOD, LOGIN, IP)
+        print(MSG)
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((IP, PORT))
+    
+    my_socket.send(bytes(MSG, 'utf-8'))
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+    data = my_socket.recv(1024)
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+    print(data.decode('utf-8'))
+    print("Finishing socket...")
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
-
-print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
-
-# Cerramos todo
-my_socket.close()
-print("Fin.")
+    my_socket.close()
+    print("End.")
