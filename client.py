d356cb65bef97c4a886b240a2de94bf09a8b38ee
muris2016 <rsemuris2016@gmail.com>
2015-11-30 09:55:57 +0100

    Errores capturados

--- client.py
+++ client.py
@@ -7,25 +7,31 @@ Programa cliente que abre un socket a un servidor
 import socket
 import sys
 
-if __name__ == "__main__":
-    METHOD = sys.argv[1]
-    LOGIN = sys.argv[2].split('@')[0]
-    IP = sys.argv[2].split('@')[1].split(':')[0]
-    PORT = int(sys.argv[2].split(':')[-1])
 
-    if METHOD == 'INVITE' or METHOD == 'BYE':
-        MSG = '%s sip:%s@%s SIP/2.0\r\n\r\n' % (METHOD, LOGIN, IP)
-        print(MSG)
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((IP, PORT))
-    
+if len(sys.argv) != 3:
+    sys.exit('Usage: python client.py method receiver@IP:SIPport')
+
+METHOD = sys.argv[1]
+LOGIN = sys.argv[2].split('@')[0]
+IP = sys.argv[2].split('@')[1].split(':')[0]
+PORT = int(sys.argv[2].split(':')[-1])
+
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((IP, PORT))
+
+MSG = '%s sip:%s@%s SIP/2.0\r\n\r\n' % (METHOD, LOGIN, IP)
+print(MSG)
+my_socket.send(bytes(MSG, 'utf-8'))
+
+data = my_socket.recv(1024).decode('utf-8')
+
+print(data)
+if '100 Trying' in data and '180 Ring' and data and '200 OK' in data:
+    MSG = 'ACK sip:%s@%s SIP/2.0\r\n\r\n' % (LOGIN, IP)
     my_socket.send(bytes(MSG, 'utf-8'))
 
-    data = my_socket.recv(1024)
+print("Finishing socket...")
 
-    print(data.decode('utf-8'))
-    print("Finishing socket...")
-
-    my_socket.close()
-    print("End.")
+my_socket.close()
+print("End.")
