360b42dada4370606bcaf45edf7077af27d58d93
muris2016 <rsemuris2016@gmail.com>
2015-11-30 09:56:38 +0100

    Errores capturados

--- server.py
+++ server.py
@@ -6,26 +6,51 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import os
+
 
 class EchoHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
+        audio_file = sys.argv[3]
         while 1:
-            line = self.rfile.read().decode('utf-8').split()
+            line = self.rfile.read().decode('utf-8')
             if not line:
                 break
-            if 'INVITE' in line:
-                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\nSIP/2.0 180 Ring\r\n\r\nSIP/2.0 200 OK\r\n\r\n")
-            elif 'ACK' in line:
-                print("aqiuiii")
-                print(line)
+            print(line)
+            if len(line.split()) != 3:
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+                break
 
+            method = line.split()[0]
+            sip_login_ip = line.split()[1]
+            sip_version = line.split()[2]
+
+            if not 'sip:' in sip_login_ip or not '@' in sip_login_ip:
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+                break
+            if sip_version != 'SIP/2.0':
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+                break
+
+            if method == 'INVITE':
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n"
+                                 + b"SIP/2.0 180 Ring\r\n\r\n"
+                                 + b"SIP/2.0 200 OK\r\n\r\n")
+            elif method == 'BYE':
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            elif method == 'ACK':
+                os.system('./mp32rtp -i 127.0.0.1 -p 23032 < ' + audio_file)
+            else:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        sys.exit('Usage: python server.py IP port audio_file')
+    if not os.path.exists(sys.argv[3]):
+        sys.exit("File %s doesn't exist" % (sys.argv[3]))
     IP = sys.argv[1]
     PORT = int(sys.argv[2])
     serv = socketserver.UDPServer((IP, PORT), EchoHandler)
